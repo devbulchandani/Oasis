804d99d9e6bdf763d2f8e84e379ddab505b2f436
Dev Bulchandani <devbulchandani8@gmail.com>
2024-03-06 16:36:16 +0530

    Fixed /model/user error

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express"
 import { validationResult } from "express-validator"
-import User from "../models/User";
+import User from "../models/user.ts";
 import bcrypt from "bcryptjs"
 import jwt from 'jsonwebtoken';
 
--- server/src/controllers/users.ts
+++ server/src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import User from '../models/User';
+import User from "../models/user.ts";
 import jwt from 'jsonwebtoken';
 import { validationResult } from 'express-validator';
 
